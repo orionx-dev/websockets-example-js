5a9634c6b6b6a6c50a5c7d22b3507aba0e2de09b
Joaquin Diaz <joaquin.diaz@ies.cl>
2020-02-14 15:29:32 -0300

    export functions in a different way

--- src/helpers/orderbook/state.js
+++ src/helpers/orderbook/state.js
@@ -1,16 +1,11 @@
 let orderbook = {}
 
-function set (newOrderbook) {
+export function set (newOrderbook) {
   orderbook = newOrderbook
   console.log('new orderbook')
   console.log(newOrderbook)
 }
 
-function get () {
+export function get () {
   return orderbook
-}
-
-export default {
-  get,
-  set
 }
