95d93dc8f348664534ac33df47b3af6957d84713
Joaquin Diaz <joaquin.diaz@ies.cl>
2020-02-14 15:30:10 -0300

    fixed endpoint

--- src/helpers/orionx/index.js
+++ src/helpers/orionx/index.js
@@ -3,7 +3,7 @@ import Orionx from 'orionx-sdk'
 Orionx.setCredentials({
   apiKey: '<apiKey>',
   secretKey: '<secretKey>',
-  apiUri: 'https://api2.orionx.com>'
+  apiUri: 'https://api2.orionx.com/graphql'
 })
 
 export default Orionx
