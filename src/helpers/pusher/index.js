db7c6838eda2ce1bf1804ac5e194c75a313ef86f
Joaquin Diaz <joaquin.diaz@ies.cl>
2020-05-06 20:30:44 -0400

    added real realtime auth endpoint

--- src/helpers/pusher/index.js
+++ src/helpers/pusher/index.js
@@ -3,7 +3,7 @@ import Pusher from 'pusher-js'
 const pusher = new Pusher('ce386deb63691b2f671c', {
   cluster: 'us2',
   forceTLS: true,
-  authEndpoint: 'http://localhost:3030'
+  authEndpoint: 'https://realtime.orionx.com/auth'
 })
 
 export default pusher
