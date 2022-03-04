484e9af025c4f4936a0f4b385867bcf3f7ee8faa
Joaquin Diaz <joaquin.diaz@ies.cl>
2022-03-04 05:04:21 -0300

    myTrades updated

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import orderbook from './public/orderbook'
 import trades from './public/trades'
 import myOrders from './private/myOrders'
 import myWallets from './private/myWallets'
+import myTrades from './private/myTrades'
 
 myOrders()
 myWallets()
 orderbook()
 trades()
+myTrades()
--- src/private/myTrades/index.js
+++ src/private/myTrades/index.js
@@ -3,10 +3,12 @@ import update from './update'
 import orionx from '../../helpers/orionx'
 
 export default async function () {
+  // Get my personal token
+  const { token } = await orionx.requestRealtimeToken()
+
   // Connecto to my private channel
   const channel = pusher.subscribe(`private-${token}`)
 
   // Bind to channel for updates
   channel.bind('trade', update)
-
 }
